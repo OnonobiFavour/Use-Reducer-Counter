506683b62707a060c7715cb5a0dab1340f635ec5
unknown <phayvorononobi@gmail.com>
2022-11-06 02:12:36 -1100

    first commit

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -4,7 +4,7 @@ import { NavLink as Link } from 'react-router-dom'
 const Nav = () => {
   return (
     <nav className='nav'>
-        <div className='logo-name'>Kripsy.io</div>
+        <div className='logo-name'>Kripsy.IO</div>
         <ul>
       <li>
         <Link to="/">Home</Link>
