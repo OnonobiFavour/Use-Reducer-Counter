beef5e6b24bb97ffbba645ee0b7050934a6754de
unknown <phayvorononobi@gmail.com>
2023-01-04 05:00:56 -1100

    home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,24 +1,30 @@
 import React from 'react'
-import { useNavigate } from 'react-router'
+import { useNavigate } from 'react-router';
 
 
 const Home = () => {
+  
+  // To navigate to the counter
   const Navigate = useNavigate();
   const handleclick = () =>{
+
     Navigate('/start')
+
   }
 
   return (
     <header className="header">
+        
         <article className="h1-wrapper">
-         <div className="circle"></div>
-         <h1 className="text-wrapper">
-            Create a counter with React.js
-         </h1>
-        <article className='wrap'>
-        <p className='heading-sub'>Counter App With React Hook . . . </p>
-        <button className='home-btn' onClick={handleclick}>Get started</button>
-        </article>
+           <div className="circle"></div>
+            <h1 className="text-wrapper">
+                Create a counter with React.js
+            </h1>
+            
+            <article className='wrap'>
+              <p className='heading-sub'>Counter App With React Hook . . . </p>
+               <button className='home-btn' onClick={handleclick}>Get started</button>
+           </article>
         </article>
     </header>
   )
